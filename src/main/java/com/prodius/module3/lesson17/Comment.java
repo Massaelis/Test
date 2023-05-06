084ba4ae1529135c374cc8302cdc7f6881fc27e0
valerii.popov <fabolo1234@gmail.com>
2023-05-06 22:23:58 +0300

    lesson17

---
+++ src/main/java/com/prodius/module3/lesson17/Comment.java
@@ -0,0 +1,5 @@
+package com.prodius.module3.lesson17;
+
+public class Comment {
+    private Note note; // MANY-to-ONE
+}
---
+++ src/main/java/com/prodius/module3/lesson17/Notebook.java
@@ -0,0 +1,9 @@
+package com.prodius.module3.lesson17;
+
+import java.util.List;
+
+public class Notebook {
+    private Note note; // ONE-to-ONE
+
+    private List<Note> notes; // ONE-to-MANY
+}
