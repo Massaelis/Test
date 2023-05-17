dc318f7a07d69453683525383ac878f763dec495
Oleh <prodius.oleg@icloud.com>
2023-05-17 11:39:48 +0300

    hw20 new change

--- src/main/java/com/prodius/module3/lesson20hw/CreateTable.java
+++ src/main/java/com/prodius/module3/lesson20hw/CreateTable.java
@@ -1,2 +1,93 @@
-package com.prodius.module3.lesson20hw;public class CreateTable {
+package com.prodius.module3.lesson20hw;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class CreateTable {
+    final static Connection connection = DatabaseUtil.getConnection();
+    static Statement statement = null;
+    protected final void createStudent() throws SQLException {
+        System.out.println("Creating table in selected database...");
+        statement = connection.createStatement();
+
+        String student = "CREATE TABLE IF NOT EXISTS student " +
+                " (id TEXT PRIMARY KEY, " +
+                " name VARCHAR(50), " +
+                " surname VARCHAR(50), " +
+                " date DATE, " +
+                " email VARCHAR(100), " +
+                " id_faculty TEXT, " +
+                " FOREIGN KEY (id_faculty) REFERENCES faculty(id))";
+
+        statement.executeUpdate(student);
+        System.out.println("Table successfully created...");
+    }
+
+    protected final void createCourse() throws SQLException {
+
+        System.out.println("Creating table in selected database...");
+        statement = connection.createStatement();
+
+        String course = "CREATE TABLE IF NOT EXISTS course " +
+                " (id TEXT PRIMARY KEY, " +
+                " group_name VARCHAR(100), " +
+                " date_start DATE, " +
+                " date_end DATE, " +
+                " id_faculty TEXT, " +
+                " FOREIGN KEY (id_faculty) REFERENCES faculty(id))";
+
+        statement.executeUpdate(course);
+        System.out.println("Table course created...");
+    }
+
+    protected final void createFaculty() throws SQLException {
+        System.out.println("Creating table in selected database...");
+        statement = connection.createStatement();
+
+        String faculty = "CREATE TABLE IF NOT EXISTS faculty " +
+                " (id TEXT PRIMARY KEY, " +
+                " faculty_name VARCHAR(100), " +
+                " dean VARCHAR(100))";
+
+        statement.executeUpdate(faculty);
+        System.out.println("Table faculty created...");
+    }
+
+    protected final void createResults_exam() throws SQLException {
+        System.out.println("Creating table in selected database...");
+        statement = connection.createStatement();
+
+        String results_exam = "CREATE TABLE IF NOT EXISTS results_exam " +
+                " (id_results_exam TEXT PRIMARY KEY, " +
+                " id_student TEXT, " +
+                " id_group TEXT, " +
+                " date_exam DATE, " +
+                " FOREIGN KEY (id_student) REFERENCES student(id), " +
+                " FOREIGN KEY (id_group) REFERENCES course(id), " +
+                " grade INTEGER CHECK\n" +
+                "       (grade > -1 and grade < 11))";
+
+        statement.executeUpdate(results_exam);
+        System.out.println("Table results_exam created...");
+    }
+
+    protected final void createOffsets() throws SQLException {
+        System.out.println("Creating table in selected database...");
+        statement = connection.createStatement();
+
+        String offsets = "CREATE TABLE IF NOT EXISTS offsets " +
+                " (id_offsets TEXT PRIMARY KEY, " +
+                " id_student TEXT, " +
+                " id_group TEXT, " +
+                " date_offsets DATE, " +
+                " FOREIGN KEY (id_student) REFERENCES student(id), " +
+                " FOREIGN KEY (id_group) REFERENCES course(id), " +
+                " grade VARCHAR CHECK\n" +
+                "       (grade = 'completed' or grade = 'process'))";
+
+        statement.executeUpdate(offsets);
+        System.out.println("Table offsets created...");
+    }
 }
--- src/main/java/com/prodius/module3/lesson20hw/Main.java
+++ src/main/java/com/prodius/module3/lesson20hw/Main.java
@@ -2,63 +2,20 @@ package com.prodius.module3.lesson20hw;
 
 import java.sql.*;
 
+import static com.prodius.module3.lesson20hw.CreateTable.connection;
+
 public class Main {
-    final static Connection connection = DatabaseUtil.getConnection();
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        CreateTable createTable = new CreateTable();
+        connection.setAutoCommit(false);
 
+        createTable.createFaculty();
+        createTable.createStudent();
+        createTable.createCourse();
+        createTable.createResults_exam();
+        createTable.createOffsets();
 
-        String student = "CREATE TABLE student " +
-                " (id_group TEXT PRIMARY KEY, " +
-                " name VARCHAR(50), " +
-                " surname VARCHAR(50), " +
-                " date DATE, " +
-                " email VARCHAR(100))";
-
-        String course = "CREATE TABLE course " +
-                " (id_group TEXT PRIMARY KEY, " +
-                " group_name VARCHAR(100), " +
-                " date_start DATE, " +
-                " date_end DATE)";
-
-        String faculty = "CREATE TABLE faculty " +
-                " (id_faculty TEXT PRIMARY KEY, " +
-                " faculty_name VARCHAR(100), " +
-                " dean VARCHAR(100))";
-
-        String results_exam = "CREATE TABLE results_exam " +
-                " (id_results_exam TEXT PRIMARY KEY, " +
-                " id_student TEXT, " +
-                " id_group TEXT, " +
-                " date_exam DATE, " +
-                " grade INTEGER CHECK\n" +
-                "       (grade > -1 and grade < 11))";
-
-        String offsets = "CREATE TABLE offsets " +
-                " (id_offsets TEXT PRIMARY KEY, " +
-                " id_student TEXT, " +
-                " id_group TEXT, " +
-                " date_offsets DATE, " +
-                " grade VARCHAR CHECK\n" +
-                "       (grade = 'completed' or grade = 'process'))";
-
-        createTable( student);
-        createTable( faculty);
-        createTable( course);
-        createTable( results_exam);
-        createTable( offsets);
-
+        connection.commit();
         connection.close();
-
-    }
-
-    private static void createTable( final String sql) throws SQLException {
-        Statement statement = null;
-
-        System.out.println("Creating table in selected database...");
-        statement = connection.createStatement();
-
-        statement.executeUpdate(sql);
-        System.out.println("Table successfully created...");
-
     }
 }
